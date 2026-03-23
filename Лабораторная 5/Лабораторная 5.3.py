84cdb2fa66afa20e7591e03bd2b491b6c8daaf3c
Natalay11 <yablontseva06@mail.ru>
2026-03-23 17:50:54 +0300

    Update Лабораторная 5.3.py

--- Лабораторная 5/Лабораторная 5.3.py
+++ Лабораторная 5/Лабораторная 5.3.py
@@ -2,19 +2,19 @@ import random
 import json
 import string
 
-names = ["John", "Jane", "Alex", "Emily", "Michael", "Sarah", "David", "Laura"]
-surnames = ["Doe", "Smith", "Johnson", "Brown", "Williams", "Jones", "Garcia"]
-name = random.choice(names) + " " + random.choice(surnames)
-age = random.randint(18, 100)
-email = name.lower().replace(" ", ".") + "@" + random.choice(["example.com", "test.com"])
-pwd_len = 12
-chars = string.ascii_letters + string.digits + string.punctuation
-password = ''.join(random.choice(chars) for _ in range(pwd_len))
+names = ["John", "Jane", "Alex", "Emily", "Michael", "Sarah", "David", "Laura"] # спикок имен
+surnames = ["Doe", "Smith", "Johnson", "Brown", "Williams", "Jones", "Garcia"] # спикок фамилий
+name = random.choice(names) + " " + random.choice(surnames) # генерируется полное имя, случайное имя и фамилия
+age = random.randint(18, 100) # генерируется случайный возраст до 100
+email = name.lower().replace(" ", ".") + "@" + random.choice(["example.com", "test.com"]) # имя в нижний регистр, добавляется собака и рандомный почтовик
+pwd_len = 12 # длинна пароля
+chars = string.ascii_letters + string.digits + string.punctuation # берутся асци, буквы и знаки пунктуации и цифры, склыдываю в один список
+password = ''.join(random.choice(chars) for _ in range(pwd_len)) # генерируется пароль по такому же принципу что и в 1 задании
 
-data = {"name": name, "age": age, "email": email, "password": password}
+data = {"name": name, "age": age, "email": email, "password": password} # полная информация
 
-with open("user.json", "w") as f:
-    json.dump(data, f, indent=4)
+with open("user.json", "w") as f: # окрываю файл для записи
+    json.dump(data, f, indent=4) # вписываю всю информацию о пользователе в джисон фай
 
-with open("user.json") as f:
-    print(json.dumps(json.load(f), indent=4))
+with open("user.json") as f: # октываю джисон файл, заганяется в джисон дата с 4 пробелами
+    print(json.dumps(json.load(f), indent=4)) # вывод загоняем в формат питон, превращаем в джисон строку
