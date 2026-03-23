90e2e41bb4fe4b44004c90447e2cadc8e97444d7
Natalay11 <yablontseva06@mail.ru>
2026-03-23 17:50:35 +0300

    Update Лабораторная 5.2.py

--- Лабораторная 5/Лабораторная 5.2.py
+++ Лабораторная 5/Лабораторная 5.2.py
@@ -2,11 +2,11 @@ import random
 import math
 import statistics
 
-nums = [random.randint(1, 100) for _ in range(100)]
-avg = statistics.mean(nums)
-med = statistics.median(nums)
-std = statistics.stdev(nums)
-root = math.sqrt(sum(nums))
+nums = [random.randint(1, 100) for р in range(100)] # создаем 100 случайных целых чисел
+avg = statistics.mean(nums) # среднее
+med = statistics.median(nums) # медиана
+std = statistics.stdev(nums) # стандартное отклонение
+root = math.sqrt(sum(nums)) # Округленное значение квадратного корня из суммы всех чисел.
 
 print(f"Среднее: {avg:.2f}, Медиана: {med:.2f}, "
 f"Стандартное отклонение: {std:.2f}, Корень из суммы: {root:.2f}")
